9832afe7a6bb443eeb263947a851c8ae7eec02e2
salybu <rkdmsdl93@gmail.com>
2021-08-23 21:59:02 +0900

    [FEAT] 영화아이템 무한 스크롤을 위해 API 수정

--- src/components/movie/MovieService.ts
+++ src/components/movie/MovieService.ts
@@ -1,9 +1,15 @@
 const MOVIE_API_URL = 'https://yts.mx/api/v2/list_movies.json';
 
 export default class MovieService {
-  public static async getMovies() {
-    const response = await fetch(MOVIE_API_URL);
-    const data = response.json();
-    return data;
+  public static async getMovies(page: number) {
+    const limit = 15;
+    try {
+      const response = await fetch(MOVIE_API_URL + `?limit=${limit}&page=${page}`);
+      const data = response.json();
+      return data;
+    } catch (error) {
+      // error Alert 를 띄우던지 어쩌던지
+      return null;
+    }
   }
 }
