2980b6f6f0b8fd49b8644334c004fae830252fd8
salybu <rkdmsdl93@gmail.com>
2021-08-23 21:59:43 +0900

    [FEAT] movieHook 에 observer 로직 추가

--- src/components/movie/MovieHook.ts
+++ src/components/movie/MovieHook.ts
@@ -1,18 +1,47 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getMovies, selectMovies } from 'redux/modules/movies';
 
 export const useMovieHook = () => {
   const dispatch = useDispatch();
-  const { movies, status, error } = useSelector(selectMovies);
+  const { movies, status, error, page } = useSelector(selectMovies);
+  const target = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    dispatch(getMovies);
+    // dispatch(getMovies);
+    getMovies(dispatch, page);
   }, []);
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: '20px',
+      threshold: 1,
+    };
+
+    let observer: IntersectionObserver;
+    if (target && movies?.length) {
+      // if (target) {
+      observer = new IntersectionObserver(onInterSecting, options);
+      observer.observe(target.current as Element);
+    }
+    return () => observer?.disconnect();
+  }, [movies]);
+
+  const onInterSecting = (entries: any) => {
+    entries.forEach((entry: any) => {
+      if (!entry.isIntersecting) {
+        return;
+      }
+      getMovies(dispatch, page);
+    });
+  };
 
   return {
     movies,
     status,
     error,
+    page,
+    target,
   };
 };
