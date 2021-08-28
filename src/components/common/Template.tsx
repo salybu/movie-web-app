659f048b59aeb0304a845e0dc13fb3670c856f2d
salybu <rkdmsdl93@gmail.com>
2021-08-28 22:38:13 +0900

    chore: 폴더구조 변경

--- src/components/common/Template.tsx
+++ src/components/common/Template.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './template.scss';
 
 function Template(props: any) {
   return <div className='container'>{props.children}</div>;
---
+++ src/components/movie/index.ts
@@ -0,0 +1,7 @@
+import Item from './MovieItem';
+import List from './MovieList';
+import MovieContainer from './MovieContainer';
+import MovieService from './MovieService';
+import { useMovie } from './useMovie';
+
+export { Item, List, MovieContainer, MovieService, useMovie };
