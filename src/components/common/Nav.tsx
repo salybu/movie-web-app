5abc8cafb4f58549aec071e5aa483be1263e9f96
salybu <rkdmsdl93@gmail.com>
2021-08-29 21:59:51 +0900

    feat: Nav 에 로그아웃 추가

--- src/components/common/Nav.tsx
+++ src/components/common/Nav.tsx
@@ -1,11 +1,25 @@
+import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import { selectAuth } from 'redux/modules/auth';
+import { AUTH } from 'utils/constants';
+import { storage } from 'utils/storage';
 import './common.scss';
 
 const Nav = () => {
   const history = useHistory();
+  const dispatch = useDispatch();
+  const { isLogin } = useSelector(selectAuth);
 
   const movePage = (path: string) => {
     history.push(path);
+  };
+
+  const logout = () => {
+    //   const isLogin = storage.get(AUTH);
+    // console.log(isLogin);
+    dispatch(logout);
+    storage.set(AUTH, '');
+    history.push('/signin');
   };
 
   return (
@@ -14,13 +28,17 @@ const Nav = () => {
         <div>관리자</div>
       </div>
       <ul className='nav_right nav_signin'>
-        <li
-          onClick={() => {
-            movePage('/signin');
-          }}
-        >
-          로그인
-        </li>
+        {!isLogin ? (
+          <li
+            onClick={() => {
+              movePage('/signin');
+            }}
+          >
+            로그인
+          </li>
+        ) : (
+          <li onClick={logout}>로그아웃</li>
+        )}
         <li
           onClick={() => {
             movePage('/signup');
