c225894d2258dd7dc328be6b343f699b49ad3512
salybu <rkdmsdl93@gmail.com>
2021-09-09 22:21:23 +0900

    feat: Movie API 추가, 상수 수정

--- src/components/movie/MovieService.ts
+++ src/components/movie/MovieService.ts
@@ -1,14 +1,34 @@
-const MOVIE_API_URL = 'https://yts.mx/api/v2/list_movies.json';
+const MOVIE_API_URL = 'https://yts.mx/api/v2/';
 
 export default class MovieService {
   public static async getMovies(page: number) {
     const limit = 15;
     try {
-      const response = await fetch(MOVIE_API_URL + `?limit=${limit}&page=${page}`);
+      const response = await fetch(MOVIE_API_URL + `list_movies.json?limit=${limit}&page=${page}`);
       const data = response.json();
       return data;
     } catch (error) {
       // error Alert 를 띄우던지 어쩌던지
+      return null;
+    }
+  }
+
+  public static async getMovie(id: number) {
+    try {
+      const response = await fetch(MOVIE_API_URL + `movie_details.json?movie_id=${id}`);
+      const data = response.json();
+      return data;
+    } catch (error) {
+      return null;
+    }
+  }
+
+  public static async getMovieSuggestion(id: number) {
+    try {
+      const response = await fetch(MOVIE_API_URL + `movie_suggestions.json?movie_id=${id}`);
+      const data = response.json();
+      return data;
+    } catch (error) {
       return null;
     }
   }
